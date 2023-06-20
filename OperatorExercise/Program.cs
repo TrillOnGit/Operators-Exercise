4ba8f0fddaa73ed1c324dd10f1e92f94356473ba
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-20 14:34:24 -0400

    Completed exercise 2.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*-------Exercise 1-------*/
             int a = 17;
             int b = 4;
             int quotient = a / b;
@@ -12,6 +13,16 @@
             {
                 Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
             }
+            /*-------Exercise 2-------*/
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with radius {radius} is {AreaOfCircle(radius)}!");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
